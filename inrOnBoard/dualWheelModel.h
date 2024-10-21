5e3cf20bd26ec6520633873080b0658d116f90e2
SidZal <siddharth@zalavadia.com>
2024-10-21 15:03:42 -0700

    removing ignored files

--- inrOnBoard/dualWheelModel.h
+++
@@ -1,9 +0,0 @@
-#ifndef DUALWHEELMODEL_H
-#define DUALWHEELMODEL_H
-
-#include <iostream>
-using namespace std;
-
-void dualWheelModel(double t, int n, double X[], double Xprime[], double omegaLeft, double omegaRight);
-
-#endif
