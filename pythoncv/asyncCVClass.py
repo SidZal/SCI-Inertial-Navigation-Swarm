63754e0830f571b1c7dd9ad3df327d780a089a2f
SidZal <siddharth@zalavadia.com>
2024-10-29 11:50:46 -0700

    more refactoring for openCV

--- pythoncv/asyncCVClass.py
+++ pythoncv/asyncCVClass.py
@@ -1,15 +1,73 @@
 import cv2
 import cv2.aruco as aruco
+import calc
+import numpy as np
 import asyncio
 
 class asyncCV:
 
-    def __init__(self, cap, dict_used):
+    def __init__(self, cap, dict_used, origin_id, ref_id, x_dist, y_dist):
         self.cap = cap
 
         dictionary = aruco.getPredefinedDictionary(dict_used)
         parameters = aruco.DetectorParameters()
+
         self.detector = aruco.ArucoDetector(dictionary, parameters)
+
+        self.ORIGIN_ID = origin_id
+        self.REFERENCE_ID = ref_id
+        self.X_DIST = x_dist
+        self.Y_DIST = y_dist
+
+        self.foundReferences = False
+        self.originX = -1
+        self.originY = -1
+        self.m3x = -1
+        self.m3y = -1
+        self.refAngle = -1
+        self.xFactor = -1
+        self.yFactor = -1
+
+        self.foundIndices = False
+        self.origin_index = -1
+        self.reference_index = -1
+
+    #only called if origin index or ref index are -1. finds indices of origin and reference markers
+    def findIndices(self, markerIDs):
+        # Check for reference markers
+        for i in range(len(markerIDs)):
+            if self.origin_index == -1 and markerIDs[i] == self.ORIGIN_ID:
+                self.origin_index = i
+            elif self.reference_index == -1 and markerIDs[i] == self.REFERENCE_ID:
+                self.reference_index = i
+
+        if self.origin_index != -1 and self.reference_index != -1:
+            self.foundIndices = True
+
+    #finds origin coordinaates, scaling factors, reference angle. maybe split up?
+    def findReferences(self, markerCorners):
+
+        if (self.origin_index < len(markerCorners)):
+            self.originX = markerCorners[self.origin_index][0][0][0]
+            self.originY = markerCorners[self.origin_index][0][0][1]
+        if (self.reference_index < len(markerCorners)):
+            self.m3x = markerCorners[self.reference_index][0][0][0]
+            self.m3y = markerCorners[self.reference_index][0][0][1]
+
+        dist_x = self.m3x - self.originX
+        dist_y = self.m3y - self.originY
+
+        if dist_x != 0 and dist_y != 0:
+            self.xFactor = self.X_DIST / dist_x
+            self.yFactor = self.Y_DIST / dist_y
+            print(f"Scaling Factor X: {self.xFactor} cm/pixel, Y: {self.yFactor} cm/pixel")
+
+            if (self.origin_index < len(markerCorners)):
+                self.refAngle = calc.get_angle(markerCorners[self.origin_index]) - np.radians(90)
+                print(f"Reference Angle: {np.degrees(self.refAngle)}°")
+
+            if self.refAngle != -1: #finished final step in finding references
+                self.foundReferences = True
 
     def detect(self):
         success, frame = self.cap.read()
@@ -17,4 +75,46 @@ class asyncCV:
         if success:
             markerCorners, markerIDs, _ = self.detector.detectMarkers(frame)
         else:
-            return #not sure if this is the right thing
+            return None #not sure if this is the right thing
+
+        if markerIDs is not None and not self.foundIndices:
+            self.findIndices(markerIDs)
+
+        # Record corner references if they have not been recorded
+        if not self.foundReferences and self.foundIndices:
+            self.findReferences(markerCorners)
+
+        for i in range(len(markerIDs)):
+            xCoor = yCoor = angle = -1
+
+            if self.foundReferences and markerIDs[i] not in [self.ORIGIN_ID, self.REFERENCE_ID]:
+                mc = markerCorners[i]
+
+                # Get the center of the third marker (agent)
+                centerX = np.mean(mc[0][:, 0])
+                centerY = np.mean(mc[0][:, 1])
+
+                # Find pixel difference between origin and bot, scale to cm
+                xCoor = (centerX - self.originX) * self.xFactor
+                yCoor = (centerY - self.originY) * self.yFactor
+
+                angle = calc.get_angle(markerCorners[i]) - self.refAngle
+
+                print(f"X: {xCoor:.2f} cm, Y: {yCoor:.2f} cm, Orientation: {np.degrees(angle):.2f}°")
+
+                # Poll for IMU data and write to CSV if received. 1/X -> max X Hz data
+                # success, data = bot.receiveNotification(1 / 60)
+                # if success:
+                #     # print(data)
+                #     ax, ay, az, gx, gy, gz, yaw, omegaL, omegaR = data
+                #
+                #     timeSinceStart = time.time() - start_time
+                #     print(f"Time: {timeSinceStart:.3f}")
+                #     write.writerow([timeSinceStart, int(markerIDs[i]),
+                #                             ax, ay, az, gx, gy, gz,
+                #                             yaw, omegaL, omegaR,
+                #                             xCoor, yCoor, angle])
+
+        marked_img = aruco.drawDetectedMarkers(frame, markerCorners, markerIDs)
+        return marked_img, xCoor, yCoor, angle
+
--- pythoncv/main.py
+++ pythoncv/main.py
@@ -7,6 +7,8 @@ import asyncio
 import numpy as np
 import robotClassBluePy
 
+import asyncCVClass
+
 
 # Width/Height dictates window size and resolution OpenCV will analyze
 WINDOW_WIDTH = 700
@@ -24,9 +26,9 @@ FILENAME = f"../.generateddata/{timestr}.csv"
 
 # OpenCV / ArUco Parameters
 DICT_USED = aruco.DICT_6X6_50
-dictionary = aruco.getPredefinedDictionary(DICT_USED)
-parameters = aruco.DetectorParameters()
-detector = aruco.ArucoDetector(dictionary, parameters)
+#dictionary = aruco.getPredefinedDictionary(DICT_USED)
+#parameters = aruco.DetectorParameters()
+#detector = aruco.ArucoDetector(dictionary, parameters)
 
 # Load camera
 cap = cv2.VideoCapture(0)
@@ -38,7 +40,7 @@ print(cv2.cuda.getCudaEnabledDeviceCount()) # 0 -> integrated graphics, >0 -> GP
 # ec:62:60:8e:60:16
 # a6:5d:28:70:b8:e2
 # bot = robotClassBluePy.INRbot("ec:62:60:8e:60:16", ["bac3","78d3","2bef"])
-bot = robotClassBluePy.INRbot("a6:5d:28:70:b8:e2", ["bac3","78d3","2bef"])
+#bot = robotClassBluePy.INRbot("a6:5d:28:70:b8:e2", ["bac3","78d3","2bef"])
 
 setRefAngle = False
 refAngle = 0
@@ -56,83 +58,87 @@ async def main():
     foundCorners = False
     setRefAngle = False
 
+    aCV = asyncCVClass.asyncCV(cap, DICT_USED, ORIGIN_ID, REFERENCE_ID, X_DIST, Y_DIST)
+
     while True:
-        success, frame = cap.read()
-
-        if success:
-           markerCorners, markerIDs, _ = detector.detectMarkers(frame)
-        else:
-            continue
-
-        if markerIDs is not None:
-            origin_index = reference_index = -1
-
-            # Check for reference markers
-            for i in range(len(markerIDs)):
-                if origin_index != -1 and reference_index != -1: break
-                if markerIDs[i] == ORIGIN_ID:
-                    origin_index = i
-                    continue
-                elif markerIDs[i] == REFERENCE_ID:
-                    reference_index = i
-                    continue
-
-            # Record corner references if they have not been recorded
-            if not foundCorners and origin_index != -1 and reference_index != -1:
-                m0x = markerCorners[origin_index][0][0][0]
-                m0y = markerCorners[origin_index][0][0][1]
-                m3x = markerCorners[reference_index][0][0][0]
-                m3y = markerCorners[reference_index][0][0][1]
-
-                dist_x = m3x - m0x
-                dist_y = m3y - m0y
-
-                if dist_x != 0 and dist_y != 0:
-                    xFactor = X_DIST / dist_x
-                    yFactor = Y_DIST / dist_y
-                    print(f"Scaling Factor X: {xFactor} cm/pixel, Y: {yFactor} cm/pixel")
-
-                    refAngle = calc.get_angle(markerCorners[origin_index]) - np.radians(90)
-                    print(f"Reference Angle: {np.degrees(refAngle)}°")
-
-                    foundCorners = True
-
-            for i in range(len(markerIDs)):
-                if foundCorners and markerIDs[i] not in [ORIGIN_ID, REFERENCE_ID]:
-                    mc = markerCorners[i]
-
-                    # Get the center of the third marker (agent)
-                    centerX = np.mean(mc[0][:, 0])
-                    centerY = np.mean(mc[0][:, 1])
-
-                    # Find pixel difference between origin and bot, scale to cm
-                    xCoor = (centerX - m0x) * xFactor
-                    yCoor = (centerY - m0y) * yFactor
-
-                    angle = calc.get_angle(markerCorners[i]) - refAngle
-
-                    # print(f"X: {xCoor:.2f} cm, Y: {yCoor:.2f} cm, Orientation: {np.degrees(angle):.2f}°")
-
-                    # Poll for IMU data and write to CSV if received. 1/X -> max X Hz data
-                    success, data = bot.receiveNotification(1/60)
-                    if success:
-                        #print(data)
-                        ax, ay, az, gx, gy, gz, yaw, omegaL, omegaR = data
-
-                        timeSinceStart = time.time() - start_time
-                        print(f"Time: {timeSinceStart:.3f}")
-                        write.writerow([timeSinceStart, int(markerIDs[i]),
-                                        ax, ay, az, gx, gy, gz,
-                                        yaw, omegaL, omegaR,
-                                        xCoor, yCoor, angle])
-
-        marked_img = aruco.drawDetectedMarkers(frame, markerCorners, markerIDs)
-        cv2.imshow('cam', marked_img)
-
+        marked_img, xCoor, yCoor, angle = aCV.detect()
+    #     success, frame = cap.read()
+    #
+    #     if success:
+    #        markerCorners, markerIDs, _ = detector.detectMarkers(frame)
+    #     else:
+    #         continue
+    #
+    #     if markerIDs is not None:
+    #         origin_index = reference_index = -1
+    #
+    #         # Check for reference markers
+    #         for i in range(len(markerIDs)):
+    #             if origin_index != -1 and reference_index != -1: break
+    #             if markerIDs[i] == ORIGIN_ID:
+    #                 origin_index = i
+    #                 continue
+    #             elif markerIDs[i] == REFERENCE_ID:
+    #                 reference_index = i
+    #                 continue
+    #
+    #         # Record corner references if they have not been recorded
+    #         if not foundCorners and origin_index != -1 and reference_index != -1:
+    #             m0x = markerCorners[origin_index][0][0][0]
+    #             m0y = markerCorners[origin_index][0][0][1]
+    #             m3x = markerCorners[reference_index][0][0][0]
+    #             m3y = markerCorners[reference_index][0][0][1]
+    #
+    #             dist_x = m3x - m0x
+    #             dist_y = m3y - m0y
+    #
+    #             if dist_x != 0 and dist_y != 0:
+    #                 xFactor = X_DIST / dist_x
+    #                 yFactor = Y_DIST / dist_y
+    #                 print(f"Scaling Factor X: {xFactor} cm/pixel, Y: {yFactor} cm/pixel")
+    #
+    #                 refAngle = calc.get_angle(markerCorners[origin_index]) - np.radians(90)
+    #                 print(f"Reference Angle: {np.degrees(refAngle)}°")
+    #
+    #                 foundCorners = True
+    #
+    #         for i in range(len(markerIDs)):
+    #             if foundCorners and markerIDs[i] not in [ORIGIN_ID, REFERENCE_ID]:
+    #                 mc = markerCorners[i]
+    #
+    #                 # Get the center of the third marker (agent)
+    #                 centerX = np.mean(mc[0][:, 0])
+    #                 centerY = np.mean(mc[0][:, 1])
+    #
+    #                 # Find pixel difference between origin and bot, scale to cm
+    #                 xCoor = (centerX - m0x) * xFactor
+    #                 yCoor = (centerY - m0y) * yFactor
+    #
+    #                 angle = calc.get_angle(markerCorners[i]) - refAngle
+    #
+    #                 # print(f"X: {xCoor:.2f} cm, Y: {yCoor:.2f} cm, Orientation: {np.degrees(angle):.2f}°")
+    #
+    #                 # Poll for IMU data and write to CSV if received. 1/X -> max X Hz data
+    #                 success, data = bot.receiveNotification(1/60)
+    #                 if success:
+    #                     #print(data)
+    #                     ax, ay, az, gx, gy, gz, yaw, omegaL, omegaR = data
+    #
+    #                     timeSinceStart = time.time() - start_time
+    #                     print(f"Time: {timeSinceStart:.3f}")
+    #                     write.writerow([timeSinceStart, int(markerIDs[i]),
+    #                                     ax, ay, az, gx, gy, gz,
+    #                                     yaw, omegaL, omegaR,
+    #                                     xCoor, yCoor, angle])
+    #
+    #     marked_img = aruco.drawDetectedMarkers(frame, markerCorners, markerIDs)
+        if marked_img is not None:
+            cv2.imshow('cam', marked_img)
+    #
         # 'Q' to end program
         if cv2.pollKey() & 0xFF == ord('q'):
            break
-
+    #
     cv2.destroyAllWindows()
     cap.release()
 
