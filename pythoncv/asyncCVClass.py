4617892fa6421c663a5cf744ba458f86680f715a
SidZal <siddharth@zalavadia.com>
2024-10-31 15:47:37 -0700

    Now writes to file. Only 20 Hz, not sure why as individually cam is 30 Hz, notifications are ~100 Hz

--- pythoncv/asyncCVClass.py
+++ pythoncv/asyncCVClass.py
@@ -28,12 +28,13 @@ class asyncCV:
         self.xFactor = -1
         self.yFactor = -1
 
-        self.foundIndices = False
         self.origin_index = -1
         self.reference_index = -1
 
     #only called if origin index or ref index are -1. finds indices of origin and reference markers
     def findIndices(self, markerIDs):
+        self.origin_index = self.reference_index = -1
+
         # Check for reference markers
         for i in range(len(markerIDs)):
             if markerIDs[i] == self.ORIGIN_ID:
@@ -41,82 +42,60 @@ class asyncCV:
             elif markerIDs[i] == self.REFERENCE_ID:
                 self.reference_index = i
 
-        if self.origin_index != -1 and self.reference_index != -1:
-            self.foundIndices = True
+        # Only return true if both references are found and indexed
+        return self.origin_index != -1 and self.reference_index != -1
 
     #finds origin coordinaates, scaling factors, reference angle. maybe split up?
-    def findReferences(self, markerCorners):
-
-        if (self.origin_index < len(markerCorners)):
-            self.originX = markerCorners[self.origin_index][0][0][0]
-            self.originY = markerCorners[self.origin_index][0][0][1]
-        if (self.reference_index < len(markerCorners)):
-            self.m3x = markerCorners[self.reference_index][0][0][0]
-            self.m3y = markerCorners[self.reference_index][0][0][1]
+    def findReferences(self, marker_corners):
+        if self.origin_index < len(marker_corners):
+            self.originX = marker_corners[self.origin_index][0][0][0]
+            self.originY = marker_corners[self.origin_index][0][0][1]
+        if self.reference_index < len(marker_corners):
+            self.m3x = marker_corners[self.reference_index][0][0][0]
+            self.m3y = marker_corners[self.reference_index][0][0][1]
 
         dist_x = self.m3x - self.originX
         dist_y = self.m3y - self.originY
-
+        print(f"{dist_x} {dist_y}")
         if dist_x != 0 and dist_y != 0:
             self.xFactor = self.X_DIST / dist_x
             self.yFactor = self.Y_DIST / dist_y
             print(f"Scaling Factor X: {self.xFactor} cm/pixel, Y: {self.yFactor} cm/pixel")
 
-            if (self.origin_index < len(markerCorners)):
-                self.refAngle = calc.get_angle(markerCorners[self.origin_index]) + np.radians(180)
-                print(f"Reference Angle: {np.degrees(self.refAngle)}°")
+            self.refAngle = calc.get_angle(marker_corners[self.origin_index])
+            print(f"Reference Angle: {np.degrees(self.refAngle)}°")
 
-            if self.refAngle != -1: #finished final step in finding references
-                self.foundReferences = True
+            self.foundReferences = True
 
     def detect(self):
         success, frame = self.cap.read()
+        marked_img = xCoor = yCoor = angle = None
 
         if success:
-            markerCorners, markerIDs, _ = self.detector.detectMarkers(frame)
-        else:
-            return None #not sure if this is the right thing
+            marker_corners, marker_ids, _ = self.detector.detectMarkers(frame)
 
-        marked_img = None
-        xCoor = yCoor = angle = -1
+            if marker_ids is not None:
+                if self.foundReferences:
+                    for i in range(len(marker_ids)):
+                        if marker_ids[i] not in [self.ORIGIN_ID, self.REFERENCE_ID]:
+                            mc = marker_corners[i]
 
-        if markerIDs is not None and not self.foundIndices:
-            self.findIndices(markerIDs)
+                            # Get the center of the third marker (agent)
+                            center_x = np.mean(mc[0][:, 0])
+                            center_y = np.mean(mc[0][:, 1])
 
-        # Record corner references if they have not been recorded
-        if not self.foundReferences and self.foundIndices:
-            self.findReferences(markerCorners)
+                            # Find pixel difference between origin and bot, scale to cm
+                            xCoor = (center_x - self.originX) * self.xFactor
+                            yCoor = (center_y - self.originY) * self.yFactor
+                            angle = calc.get_angle(marker_corners[i]) + self.refAngle
 
-        if markerIDs is not None:
-            for i in range(len(markerIDs)):
-                if self.foundReferences and markerIDs[i] not in [self.ORIGIN_ID, self.REFERENCE_ID]:
-                    mc = markerCorners[i]
+                            # print(f"X: {xCoor:.2f} cm, Y: {yCoor:.2f} cm, Orientation: {np.degrees(angle):.2f}°")
+                elif self.findIndices(marker_ids):
+                    self.findReferences(marker_corners)
+                else:
+                    # This will be run before reference is defined and the CV is searching for the reference markers
+                    pass
 
-                    # Get the center of the third marker (agent)
-                    centerX = np.mean(mc[0][:, 0])
-                    centerY = np.mean(mc[0][:, 1])
-
-                    # Find pixel difference between origin and bot, scale to cm
-                    xCoor = (centerX - self.originX) * self.xFactor
-                    yCoor = (centerY - self.originY) * self.yFactor
-
-                    angle = calc.get_angle(markerCorners[i]) - self.refAngle
-
-                    #print(f"X: {xCoor:.2f} cm, Y: {yCoor:.2f} cm, Orientation: {np.degrees(angle):.2f}°")
-
-                    # Poll for IMU data and write to CSV if received. 1/X -> max X Hz data
-                    # success, data = bot.receiveNotification(1 / 60)
-                    # if success:
-                    #     # print(data)
-                    #     ax, ay, az, gx, gy, gz, yaw, omegaL, omegaR = data
-                    #
-                    #     timeSinceStart = time.time() - start_time
-                    #     print(f"Time: {timeSinceStart:.3f}")
-                    #     write.writerow([timeSinceStart, int(markerIDs[i]),
-                    #                             ax, ay, az, gx, gy, gz,
-                    #                             yaw, omegaL, omegaR,
-                    #                             xCoor, yCoor, angle])
-
-            marked_img = aruco.drawDetectedMarkers(frame, markerCorners, markerIDs)
+            marked_img = aruco.drawDetectedMarkers(frame, marker_corners, marker_ids)
         return marked_img, xCoor, yCoor, angle
 
--- pythoncv/main_asyncCVBleak.py
+++ pythoncv/main_asyncCVBleak.py
@@ -14,10 +14,10 @@ WINDOW_WIDTH = 700
 WINDOW_HEIGHT = 550
 
 # ArUco Reference Markers
-ORIGIN_ID = 0
+ORIGIN_ID = 3
 X_DIST = 40
 Y_DIST = 50
-REFERENCE_ID = 3
+REFERENCE_ID = 0
 
 # Open file for data logging
 timestr = time.strftime("%Y%m%d-%H%M%S")
@@ -29,34 +29,44 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, WINDOW_WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, WINDOW_HEIGHT)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))  # changes codec to allow real-time video capture
 print(cv2.cuda.getCudaEnabledDeviceCount())  # 0 -> integrated graphics, >0 -> GPU-enabled
+cap.set(cv2.CAP_PROP_FPS, 30)
+print(cap.get(cv2.CAP_PROP_FPS))
 DICT_USED = aruco.DICT_6X6_50
 
-# ec:62:60:8e:60:16 RP2040 on turqoise cart
-# a6:5d:28:70:b8:e2 33 BLE on white cart
-# bot = robotClassBluePy.INRbot("ec:62:60:8e:60:16", ["bac3","78d3","2bef"])
-#bot = robotClassBluePy.INRbot("a6:5d:28:70:b8:e2", ["bac3","78d3","2bef"])
-
-setRefAngle = False
-refAngle = 0
-start_time = time.time()
+startTime = time.perf_counter()
 
 csvfile = open(FILENAME, "a")
 write = csv.writer(csvfile)
 
+# ec:62:60:8e:60:16 RP2040 on turqoise cart
+# a6:5d:28:70:b8:e2 33 BLE on white cart
 cartIDs = [["a6:5d:28:70:b8:e2", "bac3", "2bef", "78d3"]]
 cart1 = Cart(cartIDs[0])
 
 aCV = asyncCVClass.asyncCV(cap, DICT_USED, ORIGIN_ID, REFERENCE_ID, X_DIST, Y_DIST)
 
+newDataFlag = False
+xCoor = yCoor = angle = 0
 
 def wheel_ref_to_bytes(omega_left, omega_right):
     return omega_left.to_bytes(4, 'little', signed=True) + omega_right.to_bytes(4, 'little', signed=True)
 
 
 async def sensor_notification(cartUUID, data):
-    data = np.array(list(struct.iter_unpack('f', data)), dtype=float).flatten()
-    ax, ay, az, gx, gy, gz, yaw, omegaL, omegaR = data
-    print(yaw)
+    # CV is slower than notifications. Only write data if new CV data is ready
+    global newDataFlag
+    clock = time.perf_counter()- startTime
+    if newDataFlag:
+        newDataFlag = False
+        data = np.array(list(struct.iter_unpack('f', data)), dtype=float).flatten()
+        ax, ay, az, gx, gy, gz, yaw, omegaL, omegaR = data
+
+        write.writerow([clock,
+                        ax, ay, az, gx, gy, gz,
+                        yaw, omegaL, omegaR,
+                        xCoor, yCoor, angle])
+        # print(f"IMU Yaw: {yaw:.3f} CV Yaw: {angle:.3f}")
+        print(f"Time: {clock:.3f}")
 
 
 async def bluetooth_loop():
@@ -64,6 +74,8 @@ async def bluetooth_loop():
     async with BleakClient(cart1.address) as client:
         print("Connected")
         await client.start_notify(cart1.sensor, sensor_notification)
+        global startTime
+        startTime = time.perf_counter()
         await camera_loop()
         # client = BleakClient(cart1.address)
         # await client.connect()
@@ -75,10 +87,14 @@ async def bluetooth_loop():
 
 async def camera_loop():
     while True:
+        global xCoor, yCoor, angle
         marked_img, xCoor, yCoor, angle = aCV.detect()
 
         if marked_img is not None:
             cv2.imshow('cam', marked_img)
+            if xCoor is not None:
+                global newDataFlag
+                newDataFlag = True
 
         await asyncio.sleep(1/60)
 
